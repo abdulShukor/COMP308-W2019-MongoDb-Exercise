d2eb4dee2eb399a47660ef119dbcb3a5bd8194a6
abdulShukor <ashukoor12@gmail.com>
2019-02-08 11:32:32 -0500

    add mlab database

--- config/db.js
+++ config/db.js
@@ -1,4 +1,6 @@
 module.exports={
 
-"URI": "mongodb://localhost/abdulshukor"
+//"URI": "mongodb://localhost/abdulshukor"
+
+"URI" : "mongodb://Abdulshukor:L123456@ds018258.mlab.com:18258/mongodb-w2019"
 }
