ffae633e5592d9fd66a98ecfd66dc5e30522e664
abdulShukor <ashukoor12@gmail.com>
2019-02-08 13:04:03 -0500

    updated connections

--- config/db.js
+++ config/db.js
@@ -2,6 +2,6 @@ module.exports={
 
 //"URI": "mongodb://localhost/abdulshukor"
 
-"URI": "mongodb://Abdulshukor:L123456@ds018258.mlab.com:18258/mongodb-w2019"
+"URI": "mongodb://ashukoor12@gmail.com:L123456@ds018258.mlab.com:18258/mongodb-w2019"
 
 }
