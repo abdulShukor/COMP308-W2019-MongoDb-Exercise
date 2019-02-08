177e1079292ad51a194117958c2b8cbfcb6fc6ed
abdulShukor <ashukoor12@gmail.com>
2019-02-08 16:01:08 -0500

    created new collection mlab

--- config/db.js
+++ config/db.js
@@ -2,6 +2,6 @@ module.exports={
 
 //"URI": "mongodb://localhost/abdulshukor"
 
-"URI": "mongodb://ashukoor12@gmail.com:L123456@ds018258.mlab.com:18258/mongodb-w2019"
+"URI": "mongodb://abdul:L123456@ds050739.mlab.com:50739/comp308-mongodb"
 
 }
